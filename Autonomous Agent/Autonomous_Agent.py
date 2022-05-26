1c3f6f2efe98567995c156060ea688ab1fbe0237
swNotJoao <jclopes99@gmail.com>
2022-05-26 12:13:58 +0100

    AI

---
+++ Autonomous Agent/Autonomous_Agent.py
@@ -0,0 +1,17 @@
+from godot import exposed, export
+from godot import *
+
+
+@exposed
+class Autonomous_Agent(KinematicBody2D):
+
+	# member variables here, example:
+	a = export(int)
+	b = export(str, default='foo')
+
+	def _ready(self):
+		"""
+		Called every time the node is added to the scene.
+		Initialization here.
+		"""
+		pass
