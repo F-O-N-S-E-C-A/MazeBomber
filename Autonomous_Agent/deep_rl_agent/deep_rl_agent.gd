31fa36898260a006beeaafb0f76faf9efbd3495d
swNotJoao <jclopes99@gmail.com>
2022-11-11 10:16:18 +0100

    increase agent inventory for training

--- Autonomous_Agent/deep_rl_agent/deep_rl_agent.gd
+++ Autonomous_Agent/deep_rl_agent/deep_rl_agent.gd
@@ -8,12 +8,17 @@ var keys = []
 var damage_multiplier = 1
 var radius_multiplier = 1
 var max_speed = BASEMAXSPEED
-var max_bombs = 1
-var number_of_bombs = 1
-var big_bombs = 0
-var landMines = 0
+#var max_bombs = 1
+var max_bombs = 1000
+#var number_of_bombs = 1
+var number_of_bombs = 1000
+#var big_bombs = 0
+var big_bombs = 1000
+#var landMines = 0
+var landMines = 1000
 var speed_up_timer
-var c4 = 0
+#var c4 = 0
+var c4 = 1000
 var c4_planted = null
 var model = false
 
